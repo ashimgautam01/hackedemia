6032a2ed42a3ac2a1bba8a811466a7d27d3fc8a2
ashim <ashimgautam01@gmail.com>
2024-08-18 20:18:15 +0545

    oops task 1

--- python/OOP/Basics.py
+++ python/OOP/Basics.py
@@ -35,9 +35,70 @@ y=8
 # x=KBC('Ashim')
 
 
-class Animal:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
+# class Animal:
+#     def __init__(self):
+#         self.name=input("Enter name of animal\n")
+#         self.age=int(input("Enter age of animal\n"))
+    
+#     def printDetails(self):
+#         print(f'Name:{self.name}\nAge: {self.age}')
 
-species_1=Animal('Dog',18)
+    
+# species_1=Animal('Dog',18)
+# species_2=Animal('cat',78)
+# species_2.printDetails()
+# species_1.printDetails()
+# newsp=Animal
+# newsp.printDetails()
+
+
+class User:
+    def Welcome_User(self):
+        print(f'Welcome {self.name}\n')
+        print("1 Add note\n")
+        print("2 Display ")
+        print("3 Exit")
+        n=int(input("enter number 1 or 2\n"))
+        if n==1:
+            self.Add_Note()
+        elif n==2:
+            self.ViewNote()
+        elif n==3:
+            exit()
+        else:
+            print("invalid number")
+    def Add_Note(self):
+        print(f'Hey {self.name} Enter note here\n')
+        self.note=input("enter note here\n")
+        print("note added successfully")
+    def ViewNote(self):
+        print(f'Hey {self.name} Enter note here\n')
+        print(self.note)
+    def Signup(self):
+        self.name=input("enter name\n")
+        self.password=input("enter password\n")
+        self.mobile=int(input("enter mobile"))
+
+    def Login(self):
+        user=input("enter username")
+        passw=input("enter password")
+        if user==self.name and passw==self.password:
+            self.Welcome_User()
+        else:    
+            print("Invalid Username or Password")
+
+    def __init__(self):
+        while True:
+            print("1 Sign Up\n")
+            print("2 Login ")
+            n=int(input("enter number 1 or 2\n"))
+            if n==1:
+                self.Signup()
+            elif n==2:
+                self.Login()
+               
+            else:
+                break
+
+user=User()
+
