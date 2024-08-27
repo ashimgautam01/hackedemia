0e35116db7cdc68733d7f5dfcdca1675519dc7bc
ashim <ashimgautam01@gmail.com>
2024-08-27 22:20:11 +0545

    m

--- python/python-Basics/function.py
+++ python/python-Basics/function.py
@@ -1,52 +1,52 @@
-# x=int(input("enter a number\n"))
-# def add(x):
-#     if(x>0):
-#         print(f'{x} is a positive number\n')
-#     elif(x==0):
-#         print(f'{x} is 0')
-#     else:
-#         print(f'{x} is negative\n')
-# add(x)
+x=int(input("enter a number\n"))
+def add(x):
+    if(x>0):
+        print(f'{x} is a positive number\n')
+    elif(x==0):
+        print(f'{x} is 0')
+    else:
+        print(f'{x} is negative\n')
+add(x)
 
-# x=int(input("enter a number\n"))
-# def function(x):
-#     if(x%10==0):
-#         return f'{x} is a multiple of 10\n'
-#     else:
-#         return f'{x} is not multiple of 10\n'
-# print(function(x))
+x=int(input("enter a number\n"))
+def function(x):
+    if(x%10==0):
+        return f'{x} is a multiple of 10\n'
+    else:
+        return f'{x} is not multiple of 10\n'
+print(function(x))
 
-# x=int(input("enter a number\n"))
+x=int(input("enter a number\n"))
 
-# def even_checker(x):
-#     if(x%2==0):
-#         return "number is a even number\n",x
-#     else:
-#         return f'{x} is not a even number\n'
+def even_checker(x):
+    if(x%2==0):
+        return "number is a even number\n",x
+    else:
+        return f'{x} is not a even number\n'
 
-# print(even_checker(x))
-# print(even_checker(5))
+print(even_checker(x))
+print(even_checker(5))
 
-# num=int(input("Enter a number\n"))
-# def sum(num):
-#     if(num==0):
-#         return 0
-#     else:
-#         return num+sum(num-1)
+num=int(input("Enter a number\n"))
+def sum(num):
+    if(num==0):
+        return 0
+    else:
+        return num+sum(num-1)
     
-# print(sum(num))    
+print(sum(num))    
 
-# x=input("Enter a string\n")
+x=input("Enter a string\n")
 
-# print(x.lower())
+print(x.lower())
 
-# def TOH( n, sour, aux, des):
-#     if(n==1):
-#         print(f'move disk {n} from{sour} to {des}\n')
-#     else:
-#         TOH(n-1,sour,des,aux)
-#         print(f'move disk {n} from{sour} to {des}\n')
-#         TOH(n-1,aux,sour,des)
+def TOH( n, sour, aux, des):
+    if(n==1):
+        print(f'move disk {n} from{sour} to {des}\n')
+    else:
+        TOH(n-1,sour,des,aux)
+        print(f'move disk {n} from{sour} to {des}\n')
+        TOH(n-1,aux,sour,des)
 
 x=int(input("Enter a number\n"))
 TOH(x,'A','B','C')
